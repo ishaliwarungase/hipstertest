bf03b321a0880fad47d49171b85d95907a712fc1
ishaliwarungase <43536950+ishaliwarungase@users.noreply.github.com>
2021-02-11 18:09:47 +0530

    Update StepDefs.java

--- src/test/java/com/checkouttests/StepDefs.java
+++ src/test/java/com/checkouttests/StepDefs.java
@@ -46,7 +46,7 @@ public class StepDefs
     @When("User enters information to place order")
     public void user_enters_information_to_place_order() 
     {
-        driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+        driver.findElement(By.id("email")).sendKeys("");
         
         WebElement email = driver.findElement(By.id("email"));
         
